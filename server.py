49b115e8a365ec1b737bbec97fd861a148b8f200
anderskate <andrey.starozhilov@mail.ru>
2021-12-29 17:08:09 +0500

    Add a selection of buses depending on the coordinates of the window

--- server.py
+++ server.py
@@ -6,9 +6,26 @@ from trio_websocket import serve_websocket, open_websocket_url, ConnectionClosed
 from sys import stderr
 
 buses = {}
+bounds = {}
 
 
-async def recipient_server(request):
+def is_inside(bounds, lat, lng):
+    """"""
+    if not bounds:
+        return True
+
+    window_coordinates = bounds['data']
+    south_lat = window_coordinates['south_lat']
+    north_lat = window_coordinates['north_lat']
+    west_lng = window_coordinates['west_lng']
+    east_lng = window_coordinates['east_lng']
+
+    if south_lat <= lat <= north_lat and west_lng <= lng <= east_lng:
+        return True
+    return False
+
+
+async def get_buses_info(request):
     global buses
     ws = await request.accept()
     while True:
@@ -29,9 +46,15 @@ async def talk_to_browser(request):
         try:
             message = {
                 "msgType": "Buses",
-                "buses": list(buses.values())
+                "buses": [
+                    bus for bus in buses.values()
+                    if is_inside(bounds, bus['lat'], bus['lng'])
+                ]
             }
-            print(message)
+
+            buses_count = len(message['buses'])
+            logger.info(f'{buses_count} buses inside bounds')
+
             await ws.send_message(json.dumps(message))
             await listen_browser(ws)
             await trio.sleep(0.1)
@@ -39,20 +62,24 @@ async def talk_to_browser(request):
             break
 
 
-async def listen_browser(ws):
+async def listen_browser(ws, wait_msg_timeout=0.1):
     """"""
-    while True:
-        try:
+    global bounds
+    try:
+        with trio.fail_after(wait_msg_timeout):
             browser_coordinates = await ws.get_message()
-            logger.info(json.loads(browser_coordinates))
-        except ConnectionClosed:
-            break
+            bounds = json.loads(browser_coordinates)
+        logger.info(json.loads(browser_coordinates))
+    except ConnectionClosed:
+        pass
+    except trio.TooSlowError:
+        pass
 
 
 async def main():
     async with trio.open_nursery() as nursery:
         nursery.start_soon(
-            serve_websocket, recipient_server,
+            serve_websocket, get_buses_info,
             '127.0.0.1', 8080, None,
         )
         nursery.start_soon(
